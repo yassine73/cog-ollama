8c27377c511d7615bfdc25fdefb92a63f948627b
Yassine El-jdid <eljdidyassine@gmail.com>
2024-09-22 13:35:23 +0100

    added project

---
+++ predict.py
@@ -0,0 +1,55 @@
+from cog import BasePredictor, Input, ConcatenateIterator
+import json
+import time
+import requests
+import subprocess
+
+MODEL_NAME = "salmatrafi/acegpt:13b"
+OLLAMA_API = "http://127.0.0.1:11434"
+OLLAMA_GENERATE = OLLAMA_API + "/api/generate"
+
+class Predictor(BasePredictor):
+    def setup(self):
+        """Setup necessary resources for predictions"""
+        # Start server
+        print("Starting ollama server")
+        subprocess.Popen(["ollama", "serve"])
+        time.sleep(2)
+        print("Downloading model")
+        subprocess.check_call(["ollama", "pull", MODEL_NAME], close_fds=False)
+        # Load model
+        print("Running model")
+        subprocess.check_call(["ollama", "run", MODEL_NAME], close_fds=False)
+
+    def predict(self, prompt: str = Input(description="Input text for the model")) -> ConcatenateIterator[str]:
+        """Run a single prediction on the model and stream the output"""
+        payload = {
+            "model": MODEL_NAME,
+            "prompt": prompt,
+            "stream": True
+        }
+        headers = {
+            "Content-Type": "application/json"
+        }
+        
+        start_time = time.time()
+        
+        with requests.post(
+            OLLAMA_GENERATE,
+            headers=headers,
+            data=json.dumps(payload),
+            stream=True,
+            timeout=60
+        ) as response:
+            for line in response.iter_lines():
+                if line:
+                    try:
+                        chunk = json.loads(line)
+                        if 'response' in chunk:
+                            yield chunk['response']
+                    except json.JSONDecodeError:
+                        print("Failed to parse response chunk as JSON")
+        
+        end_time = time.time()
+        total_time = end_time - start_time
+        print("Total runtime:", total_time)
